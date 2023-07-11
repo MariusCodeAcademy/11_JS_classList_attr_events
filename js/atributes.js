9e1936c015bb072735a19218d97394105ededec3
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-11 09:55:45 +0300

    attr

--- js/atributes.js
+++ js/atributes.js
@@ -1,2 +1,17 @@
 'use strict';
 console.log('atributes.js file was loaded');
+
+// prideti title atributa h1 elementui
+// <h1 title="I am main">Atributes</h1>
+const h1El = document.querySelector('h1');
+console.log('h1El ===', h1El);
+
+// setAttribute(key, value) - universalus atributo uzdejimas
+// h1El.setAttribute('title', 'I am main');
+h1El.title = 'I am main';
+
+// <h2 id="title">Some title but not class</h2> uzdeti classe
+// specialTitle
+
+// <img id="mainImg" alt="some image"> uzdeti src atributa
+// https://picsum.photos/id/14/800/600
