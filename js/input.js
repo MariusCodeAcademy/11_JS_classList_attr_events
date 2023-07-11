2a7474d76af293d09b8ded2ef834cb5fccd29a5e
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-11 11:44:23 +0300

    ivesta reiksme turi buti uzpildyta

--- js/input.js
+++ js/input.js
@@ -11,15 +11,24 @@ const h1El = document.querySelector('h1');
 // console.log(mainInputEl, inputBtnEl);
 // console.log('outEl ===', outEl);
 
-inputBtnEl.addEventListener('click', inputHandler);
+inputBtnEl.addEventListener('click', () => {
+  /** @type {string} */
+  const inputValue = mainInputEl.value.trim();
+  console.log('inputValue ===', inputValue);
+  // ivesta reiksme turi buti uzpildyta
+  if (inputValue.length === 0) {
+    console.warn('iveksite reiksme');
+  }
+  // reikme butu min 5 simboliai
+});
 
 // input events
 // kai aktyvuojamas ivesties laukas
 mainInputEl.addEventListener('focus', () => {
-  console.log('ar tu esi fokuse???');
+  // console.log('ar tu esi fokuse???');
 });
 mainInputEl.addEventListener('blur', () => {
-  console.log('kai iseinam is sufokusuoto lauko');
+  // console.log('kai iseinam is sufokusuoto lauko');
 });
 // kai keiciam value - ivedam arba trinam
 mainInputEl.addEventListener('input', inputHandler);
@@ -30,7 +39,7 @@ salygaEl.addEventListener('change', () => {
 });
 
 function inputHandler() {
-  console.count('kai kazka vedam i inputa');
+  // console.count('kai kazka vedam i inputa');
   // atspausdinti tai kas ivedama
   /**
    * @type {string}
@@ -40,7 +49,7 @@ function inputHandler() {
   outEl.textContent = ivestaReiksme;
   // suskaiciuoti koks ivesto stringo ilgis
   const textIlgis = ivestaReiksme.length;
-  console.log('textIlgis ===', textIlgis);
+  // console.log('textIlgis ===', textIlgis);
   ilgisEl.textContent = textIlgis + ' vnt';
 
   // 2a pratimas
